21b1a15d0ff08c5e29e422c685b35ccd77cbc4a2
ratzlow <ratzlow@gmail.com>
2014-10-03 00:08:35 +0200

    Volatility interruption and changed event model
    Added logic to detect volatility interruptions (not yet integrated) and
    a communication only using events

--- src/main/java/net/tinyexch/exchange/trading/form/continuous/ContinuousTrading.java
+++ src/main/java/net/tinyexch/exchange/trading/form/continuous/ContinuousTrading.java
@@ -1,11 +1,17 @@
 package net.tinyexch.exchange.trading.form.continuous;
 
+import net.tinyexch.exchange.event.NotificationListener;
 import net.tinyexch.exchange.trading.form.TradingForm;
+import net.tinyexch.ob.SubmitType;
+import net.tinyexch.ob.price.safeguard.VolatilityInterruptionEmitter;
+import net.tinyexch.order.Order;
+import net.tinyexch.order.Trade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import static java.util.Collections.singleton;
@@ -22,12 +28,35 @@ import static net.tinyexch.exchange.trading.form.continuous.ContinuousTradingSta
 public class ContinuousTrading extends TradingForm<ContinuousTradingState> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ContinuousTrading.class);
+
+    private boolean volatilityInterrupted = false;
+
+    /** default: no op implementation */
+    private VolatilityInterruptionEmitter volatilityInterruptionEmitter = VolatilityInterruptionEmitter.NO_OP_EMITTER;
+
+
+    public ContinuousTrading( NotificationListener notificationListener ) {
+        super(notificationListener);
+    }
+
     @Override
     protected Map<ContinuousTradingState, Set<ContinuousTradingState>> getAllowedTransitions() {
         Map<ContinuousTradingState, Set<ContinuousTradingState>>allowedTransitions = new EnumMap<>(ContinuousTradingState.class);
         allowedTransitions.put(STOPPED, singleton(RUNNING));
         allowedTransitions.put(RUNNING, singleton(STOPPED));
         return allowedTransitions;
+    }
+
+
+    public void submit(Order order, SubmitType submitType) {
+
+        Optional<Trade> submitResult = getOrderbook().submit(order, submitType);
+
+        // TODO (FRa) : (FRa) : is this applicable?! too functional?
+        // check for volatility interruptions apply for auction and continuous trading
+        volatilityInterrupted = submitResult
+                .map(trade -> volatilityInterruptionEmitter.checkIndicativePrice( trade.getExecutionPrice() ))
+                .orElse(false);
     }
 
     @Override
@@ -39,9 +68,16 @@ public class ContinuousTrading extends TradingForm<ContinuousTradingState> {
     }
 
     public void start() {
-        transitionTo(ContinuousTradingState.RUNNING);
+        transitionTo( ContinuousTradingState.RUNNING);
     }
 
     @Override
     protected Logger getLogger() { return LOGGER; }
+
+
+    public void register( VolatilityInterruptionEmitter emitter ) {
+        volatilityInterruptionEmitter = emitter;
+    }
+
+    public void clearVolatilityInterruption() { volatilityInterrupted = false; }
 }
